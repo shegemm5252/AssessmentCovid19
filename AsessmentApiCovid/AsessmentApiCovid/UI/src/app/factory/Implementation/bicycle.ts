ecca75529fd4eb54eeb23a90040a31b52f9300a4
shegemm5252 <49652308+shegemm5252@users.noreply.github.com>
2021-10-15 08:15:05 +0100

    Delete AsessmentApiCovid/AsessmentApiCovid/UI/src/app/factory directory

--- AsessmentApiCovid/AsessmentApiCovid/UI/src/app/factory/Implementation/bicycle.ts
+++
@@ -1,8 +0,0 @@
-import { Vehicle } from "../IVehincle";
-
-export class Bicycle implements Vehicle {
-
-  public move(): void {
-      console.log("Moving the bicycle!")
-  }
-}
--- AsessmentApiCovid/AsessmentApiCovid/UI/src/app/factory/Implementation/car.ts
+++
@@ -1,8 +0,0 @@
-import { Vehicle } from "../IVehincle";
-
-export class Car implements Vehicle {
-
-  public move(): void {
-      console.log("Moving the car!")
-  }
-}
